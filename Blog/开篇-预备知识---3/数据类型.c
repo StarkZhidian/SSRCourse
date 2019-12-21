2b571191aaf84f645f7dd1a758afd3aaa34cbf8f
hongweiqiu <hongweiqiu@tencent.com>
2019-12-21 17:40:36 +0800

    Add blog

---
+++ Blog/开篇-预备知识---3/数据类型.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // 0b 开头的代表这是一个二进制编码数据
+	char c = 0b11000001;
+	printf("%d\n", c);
+	return 0;
+} 
---
+++ Blog/开篇-预备知识---3/数据类型2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // 0b 开头的代表这是一个二进制编码数据
+	short c = 0b1000000000000000;
+	printf("%d\n", c);
+	return 0;
+} 
