11c3a54ecc6a48bd8f603ac56f516637c1c13b0d
指点 <1532033525@qq.com>
2019-12-23 23:13:58 +0800

    Add content of blog:开篇-预备知识---3

---
+++ Blog/开篇-预备知识---3/数据类型5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * 打印出浮点数 f 在内存中的二进制编码数据 
+ */
+void showFloatBits(float f) {
+	int *fp = (int *) &f;
+	int size = sizeof(f) * 8;
+	int i;
+	for (i = 0; i < size; i++) {
+		printf("%d", (*fp) >> (31 - i) & 1);
+	}
+	printf("\n");
+} 
+
+int main() {
+    // 0b 开头的代表这是一个二进制编码数据
+	float c = 0b01000001;
+	showFloatBits(65); 
+	printf("%.128f\n", c); 
+	return 0;
+} 
